2b07e5e0dabeb26510ddeb854b6262051a267708
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-12 16:38:24 +0100

    aggiunto qualche commento finalizzazione e conclusione

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// richiesta utente numero km e età
+const userKm = prompt('Quanti km devi fare?');
+console.log(userKm);
+const userAge = prompt('Quanti anni hai?');
+console.log(userAge);
+const ticketPrice = userKm * 0.21;
+console.log(ticketPrice);
+// faccio un ciclo unico che comprende le 3 situazioni
+if ( userAge <= 17 ) {
+    // se il cliente è minorenne
+const sconto = ticketPrice - (( ticketPrice * 20 ) / 100);
+// sconto è inizializzato internamente solo se il ciclo si attiva inutile dichiararlo allesterno se non viene attivato il ciclo
+document.getElementById('user-message').innerHTML = sconto.toFixed(2);
+}
+else if ( userAge >= 65 ){
+    // se il cliente è over 65
+const sconto = ticketPrice - (( ticketPrice * 40 ) / 100);
+document.getElementById('user-message').innerHTML = sconto.toFixed(2);
+} else {
+    // caso rimanente
+    document.getElementById('user-message').innerHTML = ticketPrice.toFixed(2);
+}
