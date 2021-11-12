46e283b46220471835e7f0e59eb6d6dfbdc620be
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-12 17:36:22 +0100

    consiglio su come usare i const

--- js/script.js
+++ js/script.js
@@ -20,3 +20,12 @@ document.getElementById('user-message').innerHTML = sconto.toFixed(2);
     // caso rimanente
     document.getElementById('user-message').innerHTML = ticketPrice.toFixed(2);
 }
+
+// As a general rule, always declare a variable with const unless you know that the value will change.
+
+// Use const when you declare:
+
+// A new Array
+// A new Object
+// A new Function
+// A new RegExp
